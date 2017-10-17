07362f1654c149cc514f07b7a4a7b3351f1546e7
mitsuhori_yu <mitsuhori_y@mti.co.jp>
2017-10-17 15:38:39 +0900

    ormaの一件登録のベンチマーク完了

--- app/src/main/java/com/example/mitsuhori_y/roomormabenchmarkapp/MainActivity.java
+++ app/src/main/java/com/example/mitsuhori_y/roomormabenchmarkapp/MainActivity.java
@@ -5,8 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.mitsuhori_y.roomormabenchmarkapp.orma.OrmaRepository;
+import com.example.mitsuhori_y.roomormabenchmarkapp.orma.OrmaUserEntity;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,7 +27,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ormaRepository = new OrmaRepository(this);
+        ormaRepository = new OrmaRepository(this, new BenchMarker());
+
+        ormaResult = findViewById(R.id.orma_result);
+        roomResult = findViewById(R.id.room_result);
 
         roomBtn = findViewById(R.id.room_btn);
         roomBtn.setOnClickListener(new View.OnClickListener() {
@@ -35,8 +43,16 @@ public class MainActivity extends AppCompatActivity {
         ormaBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ormaRepository.insertOne();
-                ormaResult.setText(String.valueOf(ormaRepository.getBenchMarkTime()));
+                OrmaUserEntity user = new OrmaUserEntity("hoge@hoge.com", "hoge", "1", "15");
+                ormaRepository.getUserInsertSingle(user)
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(s -> {
+                            ormaResult.setText(String.valueOf(s));
+                        }, e -> {
+                            Toast t = Toast.makeText(MainActivity.this, "なにがしかのエラー", Toast.LENGTH_SHORT);
+                            t.show();
+                        });
             }
         });
 
--- app/src/main/java/com/example/mitsuhori_y/roomormabenchmarkapp/orma/OrmaRepository.java
+++ app/src/main/java/com/example/mitsuhori_y/roomormabenchmarkapp/orma/OrmaRepository.java
@@ -5,8 +5,6 @@ import android.content.Context;
 import com.example.mitsuhori_y.roomormabenchmarkapp.BenchMarker;
 
 import io.reactivex.Single;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.schedulers.Schedulers;
 
 /**
  * Created by mitsuhori_y on 2017/10/16.
@@ -16,39 +14,23 @@ public class OrmaRepository {
     private final OrmaDatabase ormaDB;
     private BenchMarker benchMarker;
 
-    public OrmaRepository(Context context) {
-        this.benchMarker = new BenchMarker();
+    public OrmaRepository(Context context, BenchMarker marker) {
+        this.benchMarker = marker;
         this.ormaDB = OrmaDatabase.builder(context)
                 .build();
     }
 
-    public void insertOne() {
-        OrmaUserEntity user = new OrmaUserEntity("hoge@hoge.com", "hoge", "1", "15");
-        BenchMarker benchMarker = new BenchMarker();
-        benchMarker.startBenchMark(System.currentTimeMillis());
-        getUserInsertSingle(user)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(s -> {
-
-                }, e -> {
-
-                });
-        benchMarker.endBenchMark(System.currentTimeMillis());
-    }
-
-    private Single<Long> getUserInsertSingle(OrmaUserEntity user) {
+    public Single<Long> getUserInsertSingle(OrmaUserEntity user) {
         return Single.create(s -> {
             try {
+                benchMarker.startBenchMark(System.currentTimeMillis());
                 long result = ormaDB.insertIntoOrmaUserEntity(user);
-                s.onSuccess(result);
+                benchMarker.endBenchMark(System.currentTimeMillis());
+                s.onSuccess(benchMarker.result());
             } catch (Exception e) {
                 s.onError(e);
             }
         });
     }
 
-    public long getBenchMarkTime() {
-        return benchMarker.result();
-    }
 }
